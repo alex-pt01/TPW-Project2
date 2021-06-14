2842b393c277b076b135a9cf4f0f34f5a21c9361
PedroMarques27 <57456759+PedroMarques27@users.noreply.github.com>
2021-06-14 18:08:06 +0100

    Added Details

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -3,6 +3,7 @@ import {Product} from "../Models/Product";
 import {DRFService} from "../Services/drf.service";
 import {ActivatedRoute, Params} from "@angular/router";
 import {identity} from "rxjs";
+import {HttpParams} from "@angular/common/http";
 
 @Component({
   selector: 'app-product-details',
@@ -11,18 +12,21 @@ import {identity} from "rxjs";
 })
 export class ProductDetailsComponent implements OnInit {
   product: Product = Product.newProduct();
-
+  id: string = '------------';
   constructor(private route: ActivatedRoute, private service: DRFService ) {
 
   }
 
   ngOnInit(): void {
-    this.getProduct(this.route.snapshot.paramMap.get('id'));
+    this.id = window.location.href.split("details/")[1];
+
+    this.getProduct(this.id);
   }
 
   getProduct(id:string|null): void{
     this.service.getProduct(Number(id)).subscribe((pr: Product)=>
     {
+
       this.product = pr;
     })
   }
