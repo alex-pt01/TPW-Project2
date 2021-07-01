730c0687090d923b92eea1fd64df0d00ea9acb46
PedroMarques27 <57456759+PedroMarques27@users.noreply.github.com>
2021-07-01 18:59:00 +0100

    Fixed

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 // Start the app by listening on the default Heroku port
 
 const express = require('express');
-const path = require('path');
 const app = express();
-app.use(express.static(path.join(__dirname, 'dist','tpw-project2')));
 
-app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname,'dist','tpw-project2','index.html'));});
+// Run the app by serving the static files
+// in the dist directory
+app.use(express.static(__dirname + '/dist'));
+
+// Start the app by listening on the default
+// Heroku port
 app.listen(process.env.PORT || 8080);
-
